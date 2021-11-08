c1d1cfff11ad88c678ecb5d0813fc34e347f5af9
近藤大翔 <taisho90@icloud.com>
2021-11-08 21:26:28 +0900

    bubbleと疎通確認

--- pages/api/bubble.ts
+++ pages/api/bubble.ts
@@ -1,18 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import BubbleResponse from './model/bubble/dataResponse'
 
 type Data = {
-  name: string
+  name: string,
+  results: object
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const repoUrl = "https://api.github.com/repos/deatiger/next-tutorial-torasemi/contents/posts"
+  const repoUrl = "https://try-plugin.bubbleapps.io/version-test/api/1.1/obj/student"
   const response = await fetch(repoUrl)
   const data: any = await response.json()
-  const animeNames = data.message
+  const r = new BubbleResponse(data)
 
-  res.status(200).json({name: animeNames})
+  res.status(200).json({name: r.count.toString(), results: r.results})
 }
---
+++ pages/api/model/bubble/dataResponse.ts
@@ -0,0 +1,13 @@
+export default class BubbleResponse{
+    cursor: number
+    remaining: number
+    count: number
+    results: any
+
+    constructor(json: any){
+        this.cursor = json['response']['cursor']
+        this.results = json['response']['results']
+        this.remaining = json['response']['remaining']
+        this.count = json['response']['count']
+    }
+}
