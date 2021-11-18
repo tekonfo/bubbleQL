a010c967096257faf4700192a7e8145f009bdd06
近藤大翔 <taisho90@icloud.com>
2021-11-18 22:21:14 +0900

    index.tsxをスリムに

---
+++ pages/components/pages/detailTable.tsx
@@ -0,0 +1,65 @@
+// ReactDOM.createPortal()の第一引数に指定するコンポーネントです。
+// Atomic Designのpages層に相当しますが、ただtemplatesを呼び出すだけのwrapperとなっています。
+import Head from 'next/head'
+import useSWR from 'swr'
+import Footer from '../organisms/footer'
+import Header from '../organisms/header'
+
+const fetcher = (...args: any[]) =>
+  fetch(args[0], args[1]).then(res => res.json())
+
+export default function DetailTable() {
+  return (
+    <div>
+      <Head>
+        <title>BubbleQL</title>
+        <meta
+          name="description"
+          content="This is a db client of Bubble like Airtable"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header></Header>
+
+      <main>
+        <Bubble />
+      </main>
+
+      <Footer></Footer>
+    </div>
+  )
+}
+
+function createTable(results: any) {
+  if (results.length == 0) return <div>no records</div>
+  // TODO: keyを全部の行のsetにする
+  const keys: Array<string> = Object.keys(results[0])
+  const th = keys.map(key => <th key={key}>{key}</th>)
+
+  const trs = []
+  for (const res of results) {
+    const td = keys.map(k => <td key={k}>{res[k]}</td>)
+    const tr = <tr>{td}</tr>
+    trs.push(tr)
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>{th}</tr>
+      </thead>
+      <tbody>{trs}</tbody>
+    </table>
+  )
+}
+
+function Bubble() {
+  const { data, error } = useSWR('/api/bubble', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  // データをレンダリングする
+  return createTable(data.results)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,8 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import useSWR from 'swr'
-
-const fetcher = (...args: any[]) =>
-  fetch(args[0], args[1]).then(res => res.json())
+import DetailTable from './components/pages/detailTable'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>BubbleQL</title>
-        <meta
-          name="description"
-          content="This is a db client of Bubble like Airtable"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="auto-cols-auto pt-0">
-        <Bubble />
-      </main>
-
-      <footer></footer>
-    </div>
-  )
-}
-
-function createTable(results: any) {
-  if (results.length == 0) return <div>no records</div>
-  // TODO: keyを全部の行のsetにする
-  const keys: Array<string> = Object.keys(results[0])
-  const th = keys.map(key => <th key={key}>{key}</th>)
-
-  const trs = []
-  for (const res of results) {
-    const td = keys.map(k => <td key={k}>{res[k]}</td>)
-    const tr = <tr>{td}</tr>
-    trs.push(tr)
-  }
-
-  return (
-    <table>
-      <thead>
-        <tr>{th}</tr>
-      </thead>
-      <tbody>{trs}</tbody>
-    </table>
-  )
-}
-
-function Bubble() {
-  const { data, error } = useSWR('/api/bubble', fetcher)
-
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
-
-  // データをレンダリングする
-  return createTable(data.results)
+  return <DetailTable></DetailTable>
 }
 
 export default Home
