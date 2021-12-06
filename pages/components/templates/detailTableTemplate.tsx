c8b7ee27268eee71938ff637f33de336b10a30f0
近藤大翔 <taisho90@icloud.com>
2021-12-06 14:03:35 +0900

    react-table作成

--- pages/components/templates/detailTableTemplate.tsx
+++ pages/components/templates/detailTableTemplate.tsx
@@ -1,10 +1,41 @@
+import { useTable, Column } from 'react-table'
 import BubbleTable from '../organisms/bubbleTable'
 import Footer from '../organisms/footer'
 import MetaHead from '../organisms/head'
 import Header from '../organisms/header'
 import Main from '../organisms/main'
 
+const columns: Column<Data>[] = [
+  {
+    Header: '名前',
+    accessor: 'name',
+  },
+  {
+    Header: '年齢',
+    accessor: 'age',
+  },
+]
+
+interface Data {
+  name: string
+  age: number
+}
+
+const data: Data[] = [
+  {
+    name: 'John',
+    age: 23,
+  },
+  {
+    name: 'Jane',
+    age: 26,
+  },
+]
+
 export default function DetailTableTemplate() {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable<Data>({ columns, data })
+
   return (
     <div>
       <MetaHead />
@@ -16,6 +47,33 @@ export default function DetailTableTemplate() {
       </Main>
 
       <Footer />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            // eslint-disable-next-line react/jsx-key
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                // eslint-disable-next-line react/jsx-key
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  // eslint-disable-next-line react/jsx-key
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
