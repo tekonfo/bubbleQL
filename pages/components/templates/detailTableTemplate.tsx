2af2711aa421b9f19296c47add8c7ef940b911db
近藤大翔 <taisho90@icloud.com>
2021-12-14 13:06:36 +0900

    テーブル出るようになった！！！

--- pages/components/templates/detailTableTemplate.tsx
+++ pages/components/templates/detailTableTemplate.tsx
@@ -1,4 +1,5 @@
-import { useTable, Column } from 'react-table'
+import React, { useMemo } from 'react'
+import { useTable } from 'react-table'
 import useSWR from 'swr'
 import { BubbleRouting } from '../../routing/routing'
 import BubbleService from '../../services/bubbleService'
@@ -7,57 +8,19 @@ import MetaHead from '../organisms/head'
 import Header from '../organisms/header'
 import Main from '../organisms/main'
 
-const columns: any = [
-  {
-    Header: '名前',
-    accessor: 'name',
-  },
-  {
-    Header: '年齢',
-    accessor: 'age',
-  },
-]
-
-const bbbb: any = [
-  {
-    name: 'John',
-    age: 23,
-  },
-  {
-    name: 'Jane',
-    age: 26,
-  },
-]
-
-// export async function getServerSideProps(context: any) {
-//   console.log('hogehoge')
-//   const routing = new BubbleRouting()
-//   const res = await fetch('/api/bubble')
-//   const data = await res.json()
-//   console.log(res)
-//   const bubbleBasicData = routing.returnBubbleBasicData(res)
-
-//   return {
-//     props: { data: 'new' }, // will be passed to the page component as props
-//   }
-// }
-
-// export async function getStaticProps() {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-//   const posts = await res.json()
-//   console.log(posts)
-//   return { props: { posts } }
-// }
-
 export default function DetailTableTemplate({ posts }: any) {
   const routing = new BubbleRouting()
   const { data, error } = useSWR(routing.route(), routing.fetcher)
 
   const bubbleService = new BubbleService()
-  const columns = bubbleService.getKeys(data?.results)
-  const body = bubbleService.getBody(data?.results)
+  const columnsData = bubbleService.getKeys(data?.results)
+  const bodyData = bubbleService.getBody(data?.results)
+
+  const columns = useMemo(() => columnsData, [])
+  const resData = useMemo(() => bodyData, [])
+
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data: body })
+    useTable({ columns, data: resData })
 
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
@@ -71,7 +34,7 @@ export default function DetailTableTemplate({ posts }: any) {
       <Main></Main>
 
       <Footer />
-      {/* <table {...getTableProps()}>
+      <table {...getTableProps()}>
         <thead>
           {headerGroups.map(headerGroup => (
             // eslint-disable-next-line react/jsx-key
@@ -97,7 +60,7 @@ export default function DetailTableTemplate({ posts }: any) {
             )
           })}
         </tbody>
-      </table> */}
+      </table>
     </div>
   )
 }
