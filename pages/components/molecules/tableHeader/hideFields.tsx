d82f4d6875b54210f1244132b74800ab272b9fd9
近藤大翔 <taisho90@icloud.com>
2021-12-16 17:25:16 +0900

    とりあえずmaterialUIいんす取る

--- pages/components/molecules/tableHeader/hideFields.tsx
+++ pages/components/molecules/tableHeader/hideFields.tsx
@@ -1,7 +1,54 @@
+import { Dialog, DialogTitle, Typography } from '@mui/material'
+import Button from '@mui/material/Button'
+import React from 'react'
+
+const emails = ['username@gmail.com', 'user02@gmail.com']
+
 export default function HideFields({
   children,
 }: {
   children?: React.ReactNode
 }) {
-  return <div className="flex-auto">Hide fields</div>
+  const [open, setOpen] = React.useState(false)
+  const [selectedValue, setSelectedValue] = React.useState(emails[1])
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = (value: string) => {
+    setOpen(false)
+    setSelectedValue(value)
+  }
+
+  return (
+    <div className="flex-auto">
+      <Button onClick={handleClickOpen}>Hide Fields</Button>
+      <SimpleDialog
+        selectedValue={selectedValue}
+        open={open}
+        onClose={handleClose}
+      />
+    </div>
+  )
+}
+
+export interface SimpleDialogProps {
+  open: boolean
+  selectedValue: string
+  onClose: (value: string) => void
+}
+
+function SimpleDialog(props: SimpleDialogProps) {
+  const { onClose, selectedValue, open } = props
+
+  const handleClose = () => {
+    onClose(selectedValue)
+  }
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>Set backup account</DialogTitle>
+    </Dialog>
+  )
 }
