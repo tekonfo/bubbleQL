b0c86f5d004d52fc9191bd2f7b0072dd332094b5
近藤大翔 <taisho90@icloud.com>
2022-01-23 12:03:58 +0900

    stateをページ単位で管理

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ApplicationIndexTemplate from '../src/components/templates/applicationIndexTemplate'
+import {
+  BuildIsRefreshBubbleTableContext,
+  IsRefreshBubbleTableContext,
+} from '../src/store/refreshBubbleTableContext'
 
 const Home = () => {
+  // TODO: ほんとはBubbleTableではないのでまずいが、refreshするだけなのでとりあえず使っている。
+  // interfaceで共通化するべき？
+  const isRefreshBubbleTableTypeContextValue =
+    BuildIsRefreshBubbleTableContext()
+
   return (
     <>
-      <ApplicationIndexTemplate />
+      <IsRefreshBubbleTableContext.Provider
+        value={isRefreshBubbleTableTypeContextValue}
+      >
+        <ApplicationIndexTemplate />
+      </IsRefreshBubbleTableContext.Provider>
     </>
   )
 }
